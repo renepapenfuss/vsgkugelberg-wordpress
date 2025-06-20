591df4d9a729eb61460c66c74c6fcae5f2194f99
René Papenfuß <rene.papenfuss@getaway.group>
2025-06-20 15:21:46 +0200

    add: create departments query pattern

---
+++ wp-content/themes/vsgkugelberg/patterns/departments-query.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Title: Departments Query
+ * Slug: vsgkugelberg/departments-query
+ * Categories: vsgkugelberg/patterns
+ */
+?>
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group">
+    <!-- wp:query {"queryId":43,"query":{"perPage":4,"pages":0,"offset":0,"postType":"department","order":"asc","orderBy":"title","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[],"format":[]}} -->
+    <div class="wp-block-query">
+        <!-- wp:post-template {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"grid","columnCount":4,"minimumColumnWidth":null}} -->
+            <!-- wp:post-featured-image /-->
+            <!-- wp:post-title {"textAlign":"center"} /-->
+            <!-- wp:post-excerpt {"textAlign":"center"} /-->
+        <!-- /wp:post-template -->
+    </div>
+    <!-- /wp:query -->
+</div>
+<!-- /wp:group -->
+ 
