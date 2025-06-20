e3d7e109aa2428b65fdf5f5be6407e5a24164098
René Papenfuß <rene.papenfuss@getaway.group>
2025-06-20 19:14:03 +0200

    add: create call to action section pattern and update index template to include it

---
+++ wp-content/themes/vsgkugelberg/patterns/section.call-to-action.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Title: Call To Action Section
+ * Slug: vsgkugelberg/section.call-to-action
+ * Categories: vsgkugelberg/sections
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"elements":{"link":{"color":{"text":"var:preset|color|primary\u002d\u002d500"}}}},"backgroundColor":"primary\u002d\u002d100","textColor":"white","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-white-color has-primary-100-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
+    <!-- wp:columns {"verticalAlignment":null} -->
+    <div class="wp-block-columns">
+        <!-- wp:column {"verticalAlignment":"center","width":"66.66%"} -->
+        <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.66%">
+            <!-- wp:heading -->
+            <h2 class="wp-block-heading">Werde Teil unserer Sportgemeinschaft!</h2>
+            <!-- /wp:heading -->
+
+            <!-- wp:paragraph -->
+            <p>Du suchst nicht nur Bewegung, sondern auch ein starkes Miteinander? Dann bist du bei uns genau richtig! Egal ob Anfänger oder Fortgeschrittener – bei uns zählt der Spaß, das Miteinander und die Freude am Sport.</p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"verticalAlignment":"center","width":"33.33%"} -->
+        <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:33.33%">
+            <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
+            <div class="wp-block-buttons">
+                <!-- wp:button {"backgroundColor":"accent\u002d\u002d100","textColor":"black","style":{"elements":{"link":{"color":{"text":"var:preset|color|black"}}}}} -->
+                <div class="wp-block-button"><a class="wp-block-button__link has-black-color has-accent-100-background-color has-text-color has-background has-link-color wp-element-button"><strong>Jetzt Mitglied werden</strong></a></div>
+                <!-- /wp:button -->
+            </div>
+            <!-- /wp:buttons -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
